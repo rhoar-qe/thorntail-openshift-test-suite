1afb11b5d4d7912a9c0a7aca14403357e9fd6f14
Juan Gonzalez <juagonza@redhat.com>
2018-09-12 15:42:47 +0200

    add topology-openshift test

---
+++ topology/topology-2/src/main/java/io/thorntail/openshift/ts/topology2/RestApplication.java
@@ -0,0 +1,8 @@
+package io.thorntail.openshift.ts.topology2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
