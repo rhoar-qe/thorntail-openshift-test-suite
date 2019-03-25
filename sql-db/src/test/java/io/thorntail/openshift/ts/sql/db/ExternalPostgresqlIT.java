1b1e62695d4bf7b6f043bf5694096d78946543ed
Ladislav Thon <lthon@redhat.com>
2019-03-25 10:55:55 +0100

    cleanup project before test class
    This commits adds an annotation `@ProjectCleanup` which, when applied
    to a test class, makes sure that the current OpenShift project
    is cleaned up before the test class is executed.
    
    All the SQL database tests use this annotation now.

--- sql-db/src/test/java/io/thorntail/openshift/ts/sql/db/ExternalPostgresqlIT.java
+++ sql-db/src/test/java/io/thorntail/openshift/ts/sql/db/ExternalPostgresqlIT.java
@@ -1,5 +1,6 @@
 package io.thorntail.openshift.ts.sql.db;
 
+import io.thorntail.openshift.ts.common.arquillian.ProjectCleanup;
 import io.thorntail.openshift.ts.sql.db.arquillian.SqlDatabaseAndConfigMap;
 import io.thorntail.openshift.ts.sql.db.infra.ExternalPostgresql;
 import org.arquillian.cube.openshift.api.OpenShiftResource;
@@ -7,6 +8,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
+@ProjectCleanup
 @SqlDatabaseAndConfigMap(ExternalPostgresql.class)
 @OpenShiftResource("file:target/classes/META-INF/fabric8/openshift.yml")
 public class ExternalPostgresqlIT extends AbstractSqlDatabaseTest {
