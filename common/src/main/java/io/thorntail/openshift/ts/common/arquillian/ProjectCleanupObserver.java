0176e1bb4a89682c5b251fddf67d403911290933
Juan Gonzalez <juagonza@redhat.com>
2019-04-10 08:20:09 +0200

    Disable fabric8 maven plugin to cleanup resources to avoid race conditions with our own resources manual deletion

--- common/src/main/java/io/thorntail/openshift/ts/common/arquillian/ProjectCleanupObserver.java
+++ common/src/main/java/io/thorntail/openshift/ts/common/arquillian/ProjectCleanupObserver.java
@@ -2,7 +2,9 @@ package io.thorntail.openshift.ts.common.arquillian;
 
 import org.arquillian.cube.kubernetes.impl.utils.CommandExecutor;
 import org.jboss.arquillian.core.api.annotation.Observes;
+import org.jboss.arquillian.test.spi.event.suite.AfterClass;
 import org.jboss.arquillian.test.spi.event.suite.BeforeClass;
+import org.jboss.arquillian.test.spi.event.suite.ClassLifecycleEvent;
 
 import java.util.logging.Logger;
 
@@ -10,6 +12,14 @@ public class ProjectCleanupObserver {
     private static final Logger log = Logger.getLogger(ProjectCleanupObserver.class.getName());
 
     public void cleanup(@Observes(precedence = 10_000) BeforeClass event) {
+        cleanUp(event);
+    }
+
+    public void cleanup(@Observes(precedence = 10_000) AfterClass event) {
+        cleanUp(event);
+    }
+
+    private void cleanUp(ClassLifecycleEvent event) {
         if (event.getTestClass().isAnnotationPresent(ProjectCleanup.class)) {
             log.info("Cleaning current OpenShift project");
             CommandExecutor cmd = new CommandExecutor();
