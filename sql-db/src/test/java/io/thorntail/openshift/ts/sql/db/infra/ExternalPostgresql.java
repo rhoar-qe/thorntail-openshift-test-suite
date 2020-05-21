72fd951cb5a4d028bfb765325227814dcb7eb533
penehyba <penehyba@redhat.com>
2020-05-21 13:50:37 +0200

    loading db image from command line TT2.6.1,latest versions mysql and postgres

--- sql-db/src/test/java/io/thorntail/openshift/ts/sql/db/infra/ExternalPostgresql.java
+++ sql-db/src/test/java/io/thorntail/openshift/ts/sql/db/infra/ExternalPostgresql.java
@@ -2,6 +2,6 @@ package io.thorntail.openshift.ts.sql.db.infra;
 
 public class ExternalPostgresql extends AbstractExternalSqlDatabaseAndConfigMapStrategy {
     public ExternalPostgresql() {
-        super("postgresql96&&geo_RDU", "target/test-classes/project-defaults-external-postgresql.yml");
+        super("postgresql115&&geo_RDU", "target/test-classes/project-defaults-external-postgresql.yml");
     }
 }
